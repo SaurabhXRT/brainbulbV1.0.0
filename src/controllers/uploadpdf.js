7ac71976177010f6175dc052ab2a239aebfe81f4
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2023-10-30 17:03:22 +0530

    Update uploadpdf.js

--- src/controllers/uploadpdf.js
+++ src/controllers/uploadpdf.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 const { google } = require("googleapis");
 const drive = google.drive("v3");
 const auth = new google.auth.GoogleAuth({
-  keyFile: "../key.json",
+  keyFile: "key.json",
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
 
