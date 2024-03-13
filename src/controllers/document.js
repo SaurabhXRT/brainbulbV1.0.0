c06364f02ef9731edeb380b681dd597ad8f3bea8
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-03-13 22:58:04 +0530

    Update document.js

--- src/controllers/document.js
+++ src/controllers/document.js
@@ -63,30 +63,24 @@ router.post("/upload", upload.single("pdf"), async (req, res) => {
     const fileMetadata = {
       name: req.file.originalname,
     };
-
     const fileStream = streamifier.createReadStream(req.file.buffer);
     const media = {
       mimeType: "application/pdf",
       body: fileStream,
     };
-
     const response = await drive.files.create({
       resource: fileMetadata,
       media,
       fields: "webViewLink, id",
     });
-    // Get the file ID from the response
     const fileId = response.data.id;
-
-    // Share the file with the specified permissions
     await drive.permissions.create({
       fileId: fileId,
       requestBody: {
-        role: "reader", // or 'writer' or 'commenter' as needed
-        type: "anyone", // This makes the file accessible to anyone with the link
+        role: "reader", 
+        type: "anyone",
       },
     });
-
     const file = new File({ pdf: response.data.webViewLink });
     await file.save();
     console.log(file);
@@ -104,34 +98,28 @@ router.get("/search", async (req, res) => {
       if (!decodedToken) {
         return res.status(401).send("Unauthorized");
       }
-  
       const username = decodedToken.username;
       const user = await User.findOne({ username });
       const searchQuery = req.query.subjectname;
-  
       const results = await File.find({
         subjectname: { $regex: searchQuery, $options: "i" },
       })
         .populate("uploadedBy", "username") // Populate the 'uploadedBy' field with the 'username' property
         .exec();
-  
       if (results.length === 0) {
-        // return res.status(404).json({ message: "No documents found for the search query." });
         return res.render("searchpdf", { displayResultsx: "block" });
       } else {
-        // Extract relevant fields from the populated 'uploadedBy' user
         const formattedResults = results.map((result) => ({
           subjectname: result.subjectname,
           filename: result.filename,
           pdf: result.pdf,
-          uploadedBy: result.uploadedBy.username, // Access the 'username' property of the populated user
+          uploadedBy: result.uploadedBy.username, 
         }));
         res.render("searchpdf", {
           formattedResults,
           user,
           displayResults: "block",
         });
-        //return res.json(formattedResults);
         return;
       }
     } catch (error) {
